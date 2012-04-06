fa8e22a4c716dc3af7e65ca4bbbe603b0b0fa5d0
Harshit Agarwal <harshita@thoughtworks.com>
2012-04-06 11:26:15 +0530

    Harshit & Duck | Maven running test

---
+++ drishti-app/src/test/java/org/ei/drishti/MyActivityTest.java
@@ -0,0 +1,18 @@
+package org.ei.drishti;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class MyActivityTest {
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+        String appName = new HelloAndroidActivity().getResources().getString(R.string.app_name);
+        assertThat(appName, equalTo("drishti-app"));
+    }
+}
