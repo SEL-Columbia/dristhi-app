608231c2b6614928d8239ce91dd1865f465aff19
Aravind SV <arvind.sv@gmail.com>
2012-08-09 10:43:15 +0530

    Harshit & Duck | Making the PNC list view work. Not with real data, yet.

---
+++ drishti-app/src/main/java/org/ei/drishti/view/controller/PNCListViewController.java
@@ -0,0 +1,25 @@
+package org.ei.drishti.view.controller;
+
+import android.content.Context;
+import org.ei.drishti.repository.AllBeneficiaries;
+import org.ei.drishti.repository.AllEligibleCouples;
+import org.ei.drishti.view.activity.PNCListActivity;
+
+public class PNCListViewController {
+    private final Context context;
+    private final AllBeneficiaries allBeneficiaries;
+    private final AllEligibleCouples allEligibleCouples;
+
+    public PNCListViewController(Context context, AllBeneficiaries allBeneficiaries, AllEligibleCouples allEligibleCouples) {
+        this.context = context;
+        this.allBeneficiaries = allBeneficiaries;
+        this.allEligibleCouples = allEligibleCouples;
+    }
+
+    public String get() {
+        return "[{\"caseId\":\"12345\",\"womanName\":\"PNC 1\",\"thaayiCardNumber\":\"TC Number 1\",\"villageName\":\"Village 1\",\"isHighRisk\":true},{\"caseId\":\"11111\",\"womanName\":\"PNC 2\",\"thaayiCardNumber\":\"TC Number 2\",\"villageName\":\"Village 2\",\"isHighRisk\":false}]";
+    }
+
+    public void startPNC(String caseId) {
+    }
+}
