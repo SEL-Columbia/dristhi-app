3f97df187a0d8c44b9d47d88318944a5940c774a
Shashank Tomar <shashant@thoughtworks.com>
2012-11-05 12:07:38 +0530

    Shashank & Kiran | Fixed bug where child details screen was showing the wrong gender

--- dristhi-app/src/main/java/org/ei/drishti/view/contract/BirthDetails.java
+++ dristhi-app/src/main/java/org/ei/drishti/view/contract/BirthDetails.java
@@ -6,11 +6,13 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class BirthDetails {
     private String age;
+    private String gender;
     private String dateOfBirth;
 
     public BirthDetails(String dateOfBirth, String age, String gender) {
         this.dateOfBirth = dateOfBirth;
         this.age = age;
+        this.gender = gender;
     }
 
     @Override
