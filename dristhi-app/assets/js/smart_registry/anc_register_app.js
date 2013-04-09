8e1f831befb0a42574ff1d2865ac245dab5677e4
Larry Weya <larryweya@gmail.com>
2013-04-09 15:29:14 +0530

    new margin styles on overview and ANC/TT

--- dristhi-app/assets/js/smart_registry/anc_register_app.js
+++ dristhi-app/assets/js/smart_registry/anc_register_app.js
@@ -215,7 +215,7 @@ angular.module("smartRegistry.controllers")
             "left_the_place": 4
         };
 
-        $scope.defaultAncServiceOption = $scope.ancServiceOptions.options[3];
+        $scope.defaultAncServiceOption = $scope.ancServiceOptions.options[0];
         $scope.serviceModeOption = $scope.defaultAncServiceOption;
 
         $scope.ancService = function (option) {
