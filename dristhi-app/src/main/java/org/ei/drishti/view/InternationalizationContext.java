0d4fa3ab37ffe688a39c6b9b20179b01e969c2fa
Harshit Agarwal <harshita@thoughtworks.com>
2013-04-23 19:27:59 +0530

    Harshit & Kiran | Fixed labels on home screen.

--- dristhi-app/src/main/java/org/ei/drishti/view/InternationalizationContext.java
+++ dristhi-app/src/main/java/org/ei/drishti/view/InternationalizationContext.java
@@ -18,11 +18,14 @@ public class InternationalizationContext {
     public String getInternationalizedLabels() {
         Map<String, String> map = new HashMap<String, String>();
         map.put("home_ec_label", resources.getString(R.string.home_ec_label));
+        map.put("home_fp_label", resources.getString(R.string.home_fp_label));
         map.put("home_anc_label", resources.getString(R.string.home_anc_label));
         map.put("home_pnc_label", resources.getString(R.string.home_pnc_label));
         map.put("home_child_label", resources.getString(R.string.home_child_label));
         map.put("home_report_label", resources.getString(R.string.home_report_label));
         map.put("home_workplan_label", resources.getString(R.string.home_workplan_label));
+        map.put("register_label", resources.getString(R.string.register_label));
+        map.put("home_videos_label", resources.getString(R.string.home_videos_label));
 
         return new Gson().toJson(map);
     }
