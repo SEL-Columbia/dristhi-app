d2d98b31476a3239cd5ea5fe52dad1723469a630
Harshit Agarwal <harshita@thoughtworks.com>
2013-04-25 18:08:35 +0530

    Harshit | Replaced jasmine-maven plugin with Karma test runner.

---
+++ dristhi-app/src/test/js/conf/karma.conf.js
@@ -0,0 +1,18 @@
+basePath = '../../../../';
+
+files = [
+    JASMINE,
+    JASMINE_ADAPTER,
+    'assets/js/jquery.min.js',
+    'assets/js/lib/*.js',
+    'assets/js/page_view.js',
+    'assets/js/anmNavigation.js',
+    'assets/js/form/*.js',
+    'assets/js/smart_registry/app.js',
+    'assets/js/smart_registry/*.js',
+    'src/test/js/lib/*.js',
+    'src/test/js/*spec.js',
+    'src/test/js/smart_registry/*spec.js'
+];
+
+autoWatch = true;
