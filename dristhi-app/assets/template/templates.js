62321eeb8e9eae53dd420713dd8baa6235a04633
Harshit Agarwal <harshita@thoughtworks.com>
2013-05-15 18:36:39 +0530

    Harshit | Removed sidepanel from reporting screens and EC list.

--- dristhi-app/assets/template/templates.js
+++ dristhi-app/assets/template/templates.js
@@ -1754,10 +1754,10 @@ function program6(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=\"navbar navbar-fixed-top affected-by-sidepanel\">\n    <div class=\"navbar-inner\">\n        <ul class=\"nav pull-left\">\n            <img class=\"sidepanel-icon affected-by-sidepanel\"><img class=\"separator\">\n            <span class=\"navbar-header\">";
+  buffer += "<div class=\"navbar navbar-fixed-top\">\n    <div class=\"navbar-inner\">\n        <ul class=\"nav pull-left\">\n            <li class=\"navbar-header\">";
   options = {hash:{},data:data};
   buffer += escapeExpression(((stack1 = helpers.monthName),stack1 ? stack1.call(depth0, depth0.month, options) : helperMissing.call(depth0, "monthName", depth0.month, options)))
-    + "</span>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content affected-by-sidepanel\">\n    <div class=\"registry-container container-fluid\">\n        ";
+    + "</li>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content\">\n    <div class=\"registry-container container-fluid\">\n        ";
   stack2 = helpers.each.call(depth0, depth0.beneficiaries, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n    </div>\n</div>\n";
@@ -1798,11 +1798,11 @@ function program1(depth0,data,depth1) {
   return buffer;
   }
 
-  buffer += "<div class=\"navbar navbar-fixed-top affected-by-sidepanel\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-full nav pull-left\">\n            <li><img class=\"sidepanel-icon affected-by-sidepanel\"><img class=\"separator\"></li>\n            <li class=\"area-8_0\">\n                <div class=\"navbar-header affected-by-sidepanel ellipsis\">";
+  buffer += "<div class=\"navbar navbar-fixed-top\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-full nav pull-left\">\n            <li class=\"area-8_0\">\n                <div class=\"navbar-header ellipsis\">";
   if (stack1 = helpers.categoryDescription) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.categoryDescription; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div>\n            </li>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content affected-by-sidepanel\">\n    <div class=\"registry-container container-fluid\">\n        <div class=\"big-text-black\">";
+    + "</div>\n            </li>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content\">\n    <div class=\"registry-container container-fluid\">\n        <div class=\"big-text-black\">";
   if (stack1 = helpers.description) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
@@ -1853,11 +1853,11 @@ function program1(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=\"navbar navbar-fixed-top affected-by-sidepanel\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-full nav pull-left\">\n            <li><img class=\"sidepanel-icon affected-by-sidepanel\"><img class=\"separator\"></li>\n            <li class=\"area-8_0\"><div class=\"navbar-header affected-by-sidepanel ellipsis\">";
+  buffer += "<div class=\"navbar navbar-fixed-top\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-full nav pull-left\">\n            <li class=\"area-8_0\"><div class=\"navbar-header ellipsis\">";
   if (stack1 = helpers.description) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.description; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "</div></li>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content affected-by-sidepanel\">\n    <div class=\"registry-container container-fluid\">\n        ";
+    + "</div></li>\n        </ul>\n    </div>\n</div>\n\n<div class=\"content\">\n    <div class=\"registry-container container-fluid\">\n        ";
   stack1 = helpers.each.call(depth0, depth0.indicatorReports, {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "\n    </div>\n</div>\n";
