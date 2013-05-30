feb06ca242d273c640752901bdd9990c45d6c6b2
Harshit Agarwal <harshita@thoughtworks.com>
2013-05-30 12:04:33 +0530

    Harshit | Updated enketo.

--- dristhi-app/assets/js/form/enketo/form.js
+++ dristhi-app/assets/js/form/enketo/form.js
@@ -847,11 +847,12 @@ function Form(formSelector, dataStr, dataStrToEdit) {
      * There is a bug in JavaRosa that has resulted in the usage of incorrect formulae on nodes inside repeat nodes.
      * Those formulae use absolute paths when relative paths should have been used. See more here:
      * https://bitbucket.org/javarosa/javarosa/wiki/XFormDeviations (point 3).
-     * Tools such as pyxform (and xls form?) also build forms in this incorrect manner. It will take time to
-     * correct this way of making forms so makeBugCompliant() aims to mimic the incorrect
-     * behaviour by injection the [position] of repeats into the XPath expressions. The resulting expression
+     * Tools such as pyxform also build forms in this incorrect manner. See https://github.com/modilabs/pyxform/issues/91
+     * It will take time to correct this so makeBugCompliant() aims to mimic the incorrect
+     * behaviour by injecting the 1-based [position] of repeats into the XPath expressions. The resulting expression
      * will then be evaluated in a way users expect (as if the paths were relative) without having to mess up
-     * the XPath Evaluator. E.g. '/data/rep_a/node_a' could become '/data/rep_a[2]/node_a' if the context is inside
+     * the XPath Evaluator.
+     * E.g. '/data/rep_a/node_a' could become '/data/rep_a[2]/node_a' if the context is inside
      * the second rep_a repeat.
      *
      * This function should be removed as soon as JavaRosa (or maybe just pyxform) fixes the way those formulae
@@ -863,14 +864,19 @@ function Form(formSelector, dataStr, dataStrToEdit) {
      * @return {string} modified expression with injected positions (1-based)
      */
     DataXML.prototype.makeBugCompliant = function (expr, selector, index) {
-        var i, repSelector, repIndex, $repParents,
+        var i, repSelector, repIndex, $collection, $wrap, $repParents,
             attr = ($form.find('[name="' + selector + '"][type="radio"]').length > 0 && index > 0) ? 'data-name' : 'name';
 
-        $repParents = form.input.getWrapNodes($form.find('[' + attr + '="' + selector + '"]')).eq(index).parents('.jr-repeat');
-        //console.debug('makeBugCompliant() received expression: '+expr+' inside repeat: '+repSelector);
+        $collection = $form.find('[' + attr + '="' + selector + '"]').filter(function () {
+            //exclude fieldset.jr-group that has child fieldset.jr-repeat with same name
+            return $(this).children('[' + attr + '="' + selector + '"]').length === 0;
+        });
+        $wrap = form.input.getWrapNodes($form.find('[' + attr + '="' + selector + '"]')).eq(index);
+        $repParents = $wrap.closest('.jr-repeat').add($wrap.parents('.jr-repeat'));
+        console.debug('makeBugCompliant() received expression: ' + expr + ' inside repeat: ' + selector);
         for (i = 0; i < $repParents.length; i++) {
             repSelector = /** @type {string} */$repParents.eq(i).attr('name');
-            //console.log(repSelector);
+            //console.log('repeat Selector: '+repSelector);
             repIndex = $repParents.eq(i).siblings('[name="' + repSelector + '"]').addBack().index($repParents.eq(i));
             console.log('calculated repeat 0-based index: ' + repIndex);
             expr = expr.replace(repSelector, repSelector + '[' + (repIndex + 1) + ']');
@@ -893,7 +899,7 @@ function Form(formSelector, dataStr, dataStrToEdit) {
      */
     DataXML.prototype.evaluate = function (expr, resTypeStr, selector, index) {
         var i, j, error, context, contextDoc, instances, id, resTypeNum, resultTypes, result, $result, attr,
-            $contextWrapNodes, $repParents;
+            $collection, $contextWrapNodes, $repParents;
         //var profiler;
         //var totTime, xTime, timeStart = new Date().getTime();
         //xpathEvalNum++;
@@ -943,9 +949,11 @@ function Form(formSelector, dataStr, dataStrToEdit) {
             /**
              * If the expression is bound to a node that is inside a repeat.... see makeBugCompliant()
              */
-            //Could consider passing a contextInsideRepeat variable to evaluate() instead
-            if ($form.find('[name="' + selector + '"]:eq(0)').closest('.jr-repeat').length > 0) {
-                //if ($form.find('[name="'+selector+'"]').parents('.jr-repeat').length > 0 ){
+                //Could consider passing a contextInsideRepeat variable to evaluate() instead
+                //if ($form.find('[name="'+selector+'"]:eq(0)').closest('.jr-repeat').length > 0){
+            $collection = $form.find('[name="' + selector + '"]');
+            if ($collection.hasClass('jr-repeat') || $collection.eq(0).closest('.jr-repeat').length > 0) {
+                console.log('going to inject position into: ' + expr + ' for context: ' + selector + ' and index: ' + index);
                 expr = this.makeBugCompliant(expr, selector, index);
             }
         }
@@ -1310,6 +1318,10 @@ function Form(formSelector, dataStr, dataStrToEdit) {
 
             if (inputType === 'radio' && name !== $node.attr('name')) {
                 $wrapNodesSameName = this.getWrapNodes($form.find('[data-name="' + name + '"]'));
+            }
+            //fieldset.jr-group wraps fieldset.jr-repeat and can have same name attribute!)
+            else if (inputType === 'fieldset' && $node.hasClass('jr-repeat')) {
+                $wrapNodesSameName = this.getWrapNodes($form.find('.jr-repeat[name="' + name + '"]'));
             }
             else {
                 $wrapNodesSameName = this.getWrapNodes($form.find('[name="' + name + '"]'));
@@ -1912,14 +1924,21 @@ function Form(formSelector, dataStr, dataStrToEdit) {
         $form.find(':not(:disabled) span.active').find(cleverSelector.join()).each(function () {
             expr = $(this).attr('data-value');
             //context = that.input.getName($(this).closest('fieldset'));
-            //context is either the sibling input (if output is inside input label),
-            //or the parent with a name attribute
-            //or the whole doc
-            $context = ($(this).siblings('[name], [data-name]').eq(0).length === 1) ? $(this).siblings('[name]:eq(0)') : $(this).closest('[name]');
+
+            /*
+             Note that in XForms input is the parent of label and in HTML the other way around so an output inside a label
+             should look at the HTML input to determine the context.
+             So, context is either the input name attribute (if output is inside input label),
+             or the parent with a name attribute
+             or the whole doc
+             */
+            $context = ($(this).parent('span').parent('label').find('[name]').eq(0).length === 1) ?
+                $(this).parent().parent().find('[name]:eq(0)') :
+                $(this).parent('span').parent('legend').parent('fieldset').find('[name]').eq(0).length === 1 ?
+                    $(this).parent().parent().parent().find('[name]:eq(0)') : $(this).closest('[name]');
             context = that.input.getName($context);
             insideRepeat = (clonedRepeatsPresent && $(this).closest('.jr-repeat').length > 0);
             insideRepeatClone = (clonedRepeatsPresent && $(this).closest('.jr-repeat.clone').length > 0);
-            //index = (insideRepeatClone) ? that.input.getIndex($(this).closest('fieldset')) : 0;
             index = (insideRepeatClone) ? that.input.getIndex($context) : 0;
 
             if (typeof outputCache[expr] !== 'undefined') {
@@ -2360,12 +2379,12 @@ function Form(formSelector, dataStr, dataStrToEdit) {
             if (!this.repeat) {
                 $form.find('input[readonly]:not([data-type-xml="geopoint"])').parent('label').each(function () {
                     //var $spans = $(this).find('span').not('.question-icons span').detach();
-                    var html = $(this).html(),
-                        relevant = $(this).find('input').attr('data-relevant'),
+                    var relevant = $(this).find('input').attr('data-relevant'),
                         branch = (relevant) ? ' jr-branch pre-init' : '',
                         name = 'name="' + $(this).find('input').attr('name') + '"',
                         attributes = (typeof relevant !== 'undefined') ? 'data-relevant="' + relevant + '" ' + name : name,
-                        value = $(this).find('input, select, textarea').val();
+                        value = $(this).find('input, select, textarea').val(),
+                        html = $(this).markdownToHtml().html();
                     $('<fieldset class="trigger' + branch + '" ' + attributes + '></fieldset>')
                         .insertBefore($(this)).append(html).append('<div class="note-value">' + value + '</div>').find('input').remove();
                     $(this).remove();
@@ -3167,6 +3186,7 @@ Date.prototype.toISOLocalString = function () {
         });
     };
 
+
     /**
      * Function: clearInputs
      *
@@ -3314,5 +3334,29 @@ Date.prototype.toISOLocalString = function () {
         return this.find(selector);
     };
 
+    $.fn.markdownToHtml = function () {
+        return this.each(function () {
+            var html,
+                $childStore = $('<div/>');
+            $(this).children().each(function (index) {
+                var name = '$$$' + index;
+                $(this).clone().markdownToHtml().appendTo($childStore);
+                $(this).replaceWith(name);
+            });
+            html = $(this).html();
+            html = html.replace(/__([^\s][^_]*[^\s])__/gm, "<strong>$1</strong>");
+            html = html.replace(/\*\*([^\s][^\*]*[^\s])\*\*/gm, "<strong>$1</strong>");
+            html = html.replace(/_([^\s][^_]*[^\s])_/gm, '<em>$1</em>');
+            html = html.replace(/\*([^\s][^\*]*[^\s])\*/gm, '<em>$1</em>');
+            //only replaces if url is valid (worthwhile feature?)
+            html = html.replace(/\[(.*)\]\(((https?:\/\/)(([\da-z\.\-]+)\.([a-z\.]{2,6})|(([0-9]{1,3}\.){3}[0-9]{1,3}))([\/\w \.\-]*)*\/?[\/\w \.\-\=\&\?]*)\)/gm, '<a href="$2">$1</a>');
+            html = html.replace(/\n/gm, '<br />');
+            $childStore.children().each(function (i) {
+                var regex = new RegExp('\\$\\$\\$' + i);
+                html = html.replace(regex, $(this)[0].outerHTML);
+            });
+            $(this).text('').append(html);
+        });
+    };
 
 })(jQuery);
