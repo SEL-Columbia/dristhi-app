8749f9cdba9571aa48eb44289fac65fbfa5d209e
Kiran <kirang20@gmail.com>
2013-06-04 10:01:15 +0530

    Kiran | Updated Enketo to get the fix the bug which broke value being populated into fields.

--- dristhi-app/assets/js/form/enketo/webform_drishti.js
+++ dristhi-app/assets/js/form/enketo/webform_drishti.js
@@ -20,54 +20,53 @@ var /**@type {Form}*/form;
 var /**@type {Connection}*/connection;
 var /**@type {*}*/fileManager;
 
-$(document).ready(function () {
-    'use strict';
-    var formParts, existingInstanceJ, instanceToEdit, loadErrors, jsonErrors, jDataO,
-        queryParams = helper.getAllQueryParams(),
-        formDataController = new FormDataController(queryParams);
+$(document).ready(function() {
+	'use strict';
+	var formParts, existingInstanceJ, instanceToEdit, loadErrors, jsonErrors, jDataO,
+		queryParams = helper.getAllQueryParams(),
+		formDataController = new FormDataController(queryParams);
 
-    connection = new Connection();
-    existingInstanceJ = formDataController.get();
+	connection = new Connection();
+	existingInstanceJ = formDataController.get();
 
-    if (!existingInstanceJ) {
-        $('form.jr').remove();
-        return gui.alert('Instance with id "' + settings.instanceId + '" could not be found.');
-    }
+	if (!existingInstanceJ){
+		$('form.jr').remove();
+		return gui.alert('Instance with id "'+settings.instanceId+'" could not be found.');
+	}
 
-    jDataO = new JData(existingInstanceJ);
-    instanceToEdit = jDataO.toXML();
-    form = new Form('form.jr:eq(0)', modelStr, instanceToEdit);
+	jDataO = new JData(existingInstanceJ);
+	instanceToEdit = jDataO.toXML();
+	form = new Form('form.jr:eq(0)', modelStr, instanceToEdit);
 
-    loadErrors = form.init();
-    //check if JSON format is complete and if not, prepend the errors
-    jsonErrors = jDataO.get().errors;
+	loadErrors = form.init();
+	//check if JSON format is complete and if not, prepend the errors
+	jsonErrors = jDataO.get().errors;
 
-    //controller for submission of data to drishti
-    $(document).on('click', 'button#submit-form:not(.disabled)', function () {
-        var jData, saveResult;
-        if (typeof form !== 'undefined') {
-            form.validateForm();
-            if (!form.isValid()) {
-                gui.alert('Form contains errors <br/>(please see fields marked in red)');
-                return;
-            }
-            else {
-                jData = jDataO.get();
-                saveResult = formDataController.save(form.getInstanceID(), jData);
-            }
-        }
-    });
+	//controller for submission of data to drishti
+	$(document).on('click', 'button#submit-form:not(.disabled)', function(){
+		var jData, saveResult;
+		if (typeof form !== 'undefined'){
+			form.validateForm();
+			if (!form.isValid()){
+				gui.alert('Form contains errors <br/>(please see fields marked in red)');
+				return;
+			}
+			else{
+				jData = jDataO.get();
+				saveResult = formDataController.save(form.getInstanceID(), jData);
+			}
+		}
+	});
 });
 
 /**
  * Connection Class (this can probably be removed - it is kept in for the google maps script only)
  * @constructor
  */
-function Connection() {
-    /**
-     * Loads a google maps API v3 script
-     * @param  {Function} callback function to call when script has been loaded and added to DOM
-     */
-    this.loadGoogleMaps = function (callback) {
-    };
+function Connection (){
+	/**
+	* Loads a google maps API v3 script
+	* @param  {Function} callback function to call when script has been loaded and added to DOM
+	*/
+	this.loadGoogleMaps = function(callback){};
 }
