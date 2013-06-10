e84601550ca17d74083a98c0ef7b220f6152d8a6
Kiran <kirang20@gmail.com>
2013-06-10 12:34:22 +0530

    Kiran | Fixed the issue of some fp methods not displaying correctly on fp register.

--- dristhi-app/assets/js/smart_registry/smart_filters.js
+++ dristhi-app/assets/js/smart_registry/smart_filters.js
@@ -1,65 +1,59 @@
 var fpMethodMap = {
     condom: 'Condom',
+    dmpa_injectable: 'DMPA/Injectable',
     iud: 'IUD',
-
+    ocp: 'OCP',
+    female_sterilization: 'Female Sterilization',
+    male_sterilization: 'Male Sterilization',
+    centchroman: 'Centchroman',
+    traditional_methods: 'Traditional Methods',
+    lam: 'LAM'
 };
+
 angular.module("smartRegistry.filters")
     .filter('humanize', function () {
-        return function(input){
-            try
-            {
+        return function (input) {
+            try {
                 var text = input.toString();
                 text = text.replace(/_/g, " ");
                 text = text.slice(0, 1).toUpperCase() + text.slice(1);
                 return text;
             }
-            catch(err)
-            {
+            catch (err) {
                 return "";
             }
         }
     })
     .filter('camelCase', function () {
-        return function(input){
-            try
-            {
+        return function (input) {
+            try {
                 var text = input.toString();
                 var texts = [];
-                input.split(" ").forEach(function(text){
+                text.split(" ").forEach(function (text) {
                     texts.push(text.slice(0, 1).toUpperCase() + text.slice(1));
                 });
                 return texts.join(" ");
             }
-            catch(err)
-            {
+            catch (err) {
                 return "";
             }
         }
     })
     .filter('fpMethodName', function () {
-        return function(input, options){
-            var method;
-            method = options.find(function(option){
-               return option['id'] === input;
-            });
-            if(method !== undefined){
-                return method['label'];
+        return function (input) {
+            if (input) {
+                return fpMethodMap[input];
             }
-            else
-            {
-                return input;
-            }
+            return input;
         }
     })
     .filter('commaSeparated', function () {
-        return function(input){
-            try
-            {
+        return function (input) {
+            try {
                 var text = input.toString().trim();
                 return text.split(/[\s]+/).join(", ");
             }
-            catch(err)
-            {
+            catch (err) {
                 return "";
             }
         }
