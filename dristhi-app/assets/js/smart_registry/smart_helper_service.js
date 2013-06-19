41cbfca39767a2b0b7bf49670019c61bb0822a5d
Larry Weya <larryweya@gmail.com>
2013-06-19 13:14:44 +0300

    Added helper to zero pad numbers

--- dristhi-app/assets/js/smart_registry/smart_helper_service.js
+++ dristhi-app/assets/js/smart_registry/smart_helper_service.js
@@ -3,6 +3,12 @@ angular.module("smartRegistry.services")
         return {
             daysBetween: function (start_date, end_date) {
                 return (end_date - start_date) / 1000 / 60 / 60 / 24;
+            },
+            zeroPad: function(num, size) {
+                if(size === undefined)
+                    size = 2;
+                var s = "00" + num;
+                return s.substr(s.length-size);
             }
         }
     });
--- dristhi-app/src/test/js/smart_registry/smart_helper_service_spec.js
+++ dristhi-app/src/test/js/smart_registry/smart_helper_service_spec.js
@@ -20,4 +20,24 @@ describe('Smart Helpers', function () {
                     new Date(Date.parse("2012-03-01")), new Date(Date.parse("2012-02-29")))).toEqual(-1);
         });
     });
+
+    describe("Zero Padding", function(){
+       it("should pad a number with a zero if the number is less than 10", function(){
+           var num = 6;
+
+           var result = smartHelper.zeroPad(num);
+           var expected = "06";
+
+           expect(result).toEqual(expected);
+       });
+
+        it("should NOT pad a number with a zero if the number is greater than 9", function(){
+            var num = 12;
+
+            var result = smartHelper.zeroPad(num);
+            var expected = "12";
+
+            expect(result).toEqual(expected);
+        });
+    });
 });
