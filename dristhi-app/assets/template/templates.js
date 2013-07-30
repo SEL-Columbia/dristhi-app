e379a764dbdb8dc11542020d1dd0ab559afe6f6e
Kiran <kirang20@gmail.com>
2013-07-30 11:30:26 +0530

    Kumaran & Kiran | #469 | Removed side panel icon from PNC profile.

--- dristhi-app/assets/template/templates.js
+++ dristhi-app/assets/template/templates.js
@@ -1079,10 +1079,10 @@ function program23(depth0,data) {
   return buffer;
   }
 
-  buffer += "<div class=\"navbar navbar-fixed-top affected-by-sidepanel\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-6_5 nav pull-left\">\n            <li><img class=\"sidepanel-icon affected-by-sidepanel\"><img class=\"separator\"></li>\n            <li class=\"area-7_5\">\n                <div class=\"navbar-header affected-by-sidepanel ellipsis\">";
+  buffer += "<div class=\"navbar navbar-fixed-top\">\n    <div class=\"navbar-inner\">\n        <ul class=\"area-6_5 nav pull-left\">\n            <li class=\"area-7_5\">\n                <div class=\"navbar-header ellipsis\">";
   options = {hash:{},data:data};
   buffer += escapeExpression(((stack1 = helpers.formatText || depth0.formatText),stack1 ? stack1.call(depth0, ((stack1 = depth0.coupleDetails),stack1 == null || stack1 === false ? stack1 : stack1.wifeName), options) : helperMissing.call(depth0, "formatText", ((stack1 = depth0.coupleDetails),stack1 == null || stack1 === false ? stack1 : stack1.wifeName), options)))
-    + "</div>\n            </li>\n        </ul>\n\n        <ul class=\"nav pull-right affected-by-sidepanel\">\n            <li><img class=\"separator\"></li>\n            <li data-modal-target=\"#pncFormModal\"><img class=\"update-data\"></li>\n        </ul>\n    </div>\n</div>\n\n<div id=\"pncFormModal\" class=\"modal-content\">\n    <div class=\"modal-row\" data-form=\"pnc_visit\" data-caseid=\"";
+    + "</div>\n            </li>\n        </ul>\n\n        <ul class=\"nav pull-right\">\n            <li><img class=\"separator\"></li>\n            <li data-modal-target=\"#pncFormModal\"><img class=\"update-data\"></li>\n        </ul>\n    </div>\n</div>\n\n<div id=\"pncFormModal\" class=\"modal-content\">\n    <div class=\"modal-row\" data-form=\"pnc_visit\" data-caseid=\"";
   if (stack2 = helpers.caseId) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = depth0.caseId; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
   buffer += escapeExpression(stack2)
@@ -1090,7 +1090,7 @@ function program23(depth0,data) {
   if (stack2 = helpers.caseId) { stack2 = stack2.call(depth0, {hash:{},data:data}); }
   else { stack2 = depth0.caseId; stack2 = typeof stack2 === functionType ? stack2.apply(depth0) : stack2; }
   buffer += escapeExpression(stack2)
-    + "\">Close PNC</div>\n</div>\n\n<div id=\"content\" class=\"content-detail affected-by-sidepanel\">\n    <div class=\"status-bar affected-by-sidepanel\">\n        ";
+    + "\">Close PNC</div>\n</div>\n\n<div id=\"content\" class=\"content-detail\">\n    <div class=\"status-bar\">\n        ";
   stack2 = helpers['if'].call(depth0, ((stack1 = depth0.coupleDetails),stack1 == null || stack1 === false ? stack1 : stack1.isInArea), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
   if(stack2 || stack2 === 0) { buffer += stack2; }
   buffer += "\n        ";
