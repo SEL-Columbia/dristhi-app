088675cff63a706c59a3e3c5c376397f90a35834
Kiran <kirang20@gmail.com>
2013-11-16 10:57:48 +0530

    Kiran | #738 | Upgraded angularjs to 1.2.1 (underscore-empathy). Fixed an issue in app.js by removing the unused variable .

--- dristhi-app/assets/js/smart_registry/app.js
+++ dristhi-app/assets/js/smart_registry/app.js
@@ -2,14 +2,10 @@ angular.module("smartRegistry.controllers", []);
 angular.module("smartRegistry.services", []);
 angular.module("smartRegistry.filters", []);
 angular.module("smartRegistry", ["ui.bootstrap", "smartRegistry.controllers", "smartRegistry.services",
-    "smartRegistry.filters", "smartRegistry.directives"]).config(
-    ['$routeProvider', '$dialogProvider', function ($routeProvider, $dialogProvider) {
-        /*$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});*/
-        $dialogProvider.options({backdropFade: false, dialogFade: false});
-    }]);
-
+        "smartRegistry.filters", "smartRegistry.directives"]).config(
+        ['$dialogProvider', function ($dialogProvider) {
+            $dialogProvider.options({backdropFade: false, dialogFade: false});
+        }]);
 if (Array.prototype.find === undefined) {
     Array.prototype.find = function (func) {
         var idx;
