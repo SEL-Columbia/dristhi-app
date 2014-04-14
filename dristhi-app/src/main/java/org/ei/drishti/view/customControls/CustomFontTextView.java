84a87810fd69f2913e689038843d3d71a1452e73
Thote <thotego@thoughtworks.com>
2014-04-14 11:39:06 +0530

    Thote | #948 | Styling the nav bar and status bar with proper fonts.

---
+++ dristhi-app/src/main/java/org/ei/drishti/view/customControls/CustomFontTextView.java
@@ -0,0 +1,41 @@
+package org.ei.drishti.view.customControls;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+import org.ei.drishti.R;
+
+public class CustomFontTextView extends TextView {
+
+    public CustomFontTextView(Context context) {
+        this(context, null, 0);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TypedArray attributes = context.obtainStyledAttributes(
+                attrs, R.styleable.org_ei_drishti_view_customControls_CustomFontTextView, defStyle, 0);
+        int variant = attributes.getInt(
+                R.styleable.org_ei_drishti_view_customControls_CustomFontTextView_fontVariant, 0);
+
+        setFontVariant(variant);
+    }
+
+    public void setFontVariant(int variant) {
+        setFontVariant(FontVariant.tryParse(variant, FontVariant.REGULAR));
+    }
+
+    public void setFontVariant(FontVariant variant) {
+        setTypeface(
+                Typeface.createFromAsset(
+                        getContext().getAssets(),
+                        variant.fontFile()));
+    }
+}
