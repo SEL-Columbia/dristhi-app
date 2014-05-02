061e0498ed2c72f6befdb30541823491686297ba
Thote <thotego@thoughtworks.com>
2014-05-02 17:06:24 +0530

    Thote | #967 | Used async task to fetch the ANM details in homescreen without disturbing UI.

---
+++ dristhi-app/src/main/java/org/ei/drishti/view/controller/NativeAfterANMDetailsFetchListener.java
@@ -0,0 +1,7 @@
+package org.ei.drishti.view.controller;
+
+import org.ei.drishti.view.contract.HomeContext;
+
+public interface NativeAfterANMDetailsFetchListener {
+    void afterFetch(HomeContext anmDetails);
+}
