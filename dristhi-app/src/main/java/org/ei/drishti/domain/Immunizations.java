f4f84cc601914d7647cb92689bfc9b54aeb6a083
Thirunavukkarasu <thirunar@thoughtworks.com>
2014-06-27 11:28:41 +0530

    Thiru | #1022 | Fixed an issue where few indicator names not being displayed in child profile screen.

--- dristhi-app/src/main/java/org/ei/drishti/domain/Immunizations.java
+++ dristhi-app/src/main/java/org/ei/drishti/domain/Immunizations.java
@@ -1,7 +1,9 @@
 package org.ei.drishti.domain;
 
 public enum Immunizations implements Displayable {
-    bcg("BCG"), opv_0("OPV 0"), hepb_0("HepB 0"), opv_1("OPV 1"), opv_2("OPV 2"), measles("Measles");
+    bcg("BCG"), opv_0("OPV 0"), hepb_0("HepB 0"), opv_1("OPV 1"), opv_2("OPV 2"), measles("Measles"),
+    pentavalent_1("Pentavalent 1"), pentavalent_2("Pentavalent 2"), opv_3("OPV 3"), pentavalent_3("Pentavalent 3"), je("JE"), mmr("MMR"),
+    dptbooster_1("DPT Booster 1"), opvbooster("OPV Booster"), measlesbooster("Measles Booster"), je_2("JE 2"), dptbooster_2("DPT Booster 2");
     private String displayValue;
 
     Immunizations(String displayValue) {
