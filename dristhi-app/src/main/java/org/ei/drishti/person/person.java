3c55a886103f889b41b1fa1d957cdcf4f78ece47
Dimas Ciputra <dimas.ciputra@gmail.com>
2015-02-12 13:08:24 +0600

    [DTC] Add person domain

--- dristhi-app/src/main/java/org/ei/drishti/person/person.java
+++ dristhi-app/src/main/java/org/ei/drishti/person/person.java
@@ -1,8 +1,32 @@
 package org.ei.drishti.person;
 
+import java.util.Map;
+
 /**
  * Created by user on 2/12/15.
  */
-public class person {
-    
+public class Person {
+    private String caseId;
+    private Map<String, String> details;
+
+    public Person(Map<String, String> details, String caseId) {
+        this.details = details;
+        this.caseId = caseId;
+    }
+
+    public String getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(String caseId) {
+        this.caseId = caseId;
+    }
+
+    public Map<String, String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(Map<String, String> details) {
+        this.details = details;
+    }
 }
