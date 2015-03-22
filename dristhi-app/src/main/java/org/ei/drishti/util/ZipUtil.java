766bf3ab10a86a30edabd95357797314e045ca5c
Dimas <dimas.ciputra@gmail.com>
2015-03-22 18:30:19 +0600

    DTC | #17 | add ZipUtil

---
+++ dristhi-app/src/main/java/org/ei/drishti/util/ZipUtil.java
@@ -0,0 +1,54 @@
+package org.ei.drishti.util;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by Dimas Ciputra on 3/21/15.
+ */
+public class ZipUtil {
+    private String zipFile;
+    private String zipLocation;
+
+    public ZipUtil(String zipFile, String zipLocation) {
+        this.zipFile = zipFile;
+        this.zipLocation = zipLocation;
+        checkDir(" ");
+    }
+
+    public void unzip() {
+        try {
+            FileInputStream fin = new FileInputStream(this.zipFile);
+            ZipInputStream zin = new ZipInputStream(fin);
+            ZipEntry ze = null;
+            while((ze = zin.getNextEntry()) != null) {
+                Log.v("ZipService: ", "Unzipping " + ze.getName());
+                if(ze.isDirectory()) {
+                    checkDir(ze.getName());
+                } else {
+                    FileOutputStream fout = new FileOutputStream(this.zipLocation + ze.getName());
+                    for (int c = zin.read(); c != -1; c = zin.read()) {
+                        fout.write(c);
+                    }
+                    zin.closeEntry();
+                    fout.close();
+                }
+            }
+            zin.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void checkDir(String location) {
+        File f = new File(this.zipLocation + location);
+        if(!f.isDirectory()) {
+            f.mkdirs();
+        }
+    }
+}
