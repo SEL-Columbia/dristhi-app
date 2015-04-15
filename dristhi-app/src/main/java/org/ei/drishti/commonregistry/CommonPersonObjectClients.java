aded8fc86091d71312b8cad57c6cc5f03af1bc7d
raihan <raihan@mpower-social.com>
2015-04-15 21:31:02 +0600

    RA|contract object list for Common Registry, added creation date and credits

--- dristhi-app/src/main/java/org/ei/drishti/commonregistry/CommonPersonObjectClients.java
+++ dristhi-app/src/main/java/org/ei/drishti/commonregistry/CommonPersonObjectClients.java
@@ -3,7 +3,7 @@ package org.ei.drishti.commonregistry;
 import org.ei.drishti.view.contract.SmartRegisterClients;
 
 /**
- * Created by user on 2/12/15.
+ * Created by Raihan Ahmed on 2/12/15.
  */
 public class CommonPersonObjectClients extends SmartRegisterClients {
 }
