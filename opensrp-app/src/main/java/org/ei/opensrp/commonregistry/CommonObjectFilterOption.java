ec79627e93e8397b9b8baa2ccd58e679f8f4e25c
raihan <raihan@mpower-social.com>
2015-06-04 15:57:34 +0600

    RA| added common filter option so that it can be used with common registry model

---
+++ opensrp-app/src/main/java/org/ei/opensrp/commonregistry/CommonObjectFilterOption.java
@@ -0,0 +1,37 @@
+package org.ei.opensrp.commonregistry;
+
+import org.ei.opensrp.view.contract.SmartRegisterClient;
+import org.ei.opensrp.view.dialog.FilterOption;
+
+public class CommonObjectFilterOption implements FilterOption {
+    private final String criteria;
+    public final String fieldname,sortOptionName;
+    ByColumnAndByDetails byColumnAndByDetails;
+
+    public enum ByColumnAndByDetails{
+        byColumn,byDetails;
+    }
+
+    public CommonObjectFilterOption(String sortOptionName ,String criteria, String fieldname, ByColumnAndByDetails byColumnAndByDetails) {
+        this.criteria = criteria;
+        this.fieldname = fieldname;
+        this.byColumnAndByDetails= byColumnAndByDetails;
+        this.sortOptionName = sortOptionName;
+    }
+
+    @Override
+    public String name() {
+        return sortOptionName;
+    }
+
+    @Override
+    public boolean filter(SmartRegisterClient client) {
+        switch (byColumnAndByDetails){
+            case byColumn:
+                return ((CommonPersonObjectClient)client).getColumnmaps().get(fieldname).contains(criteria);
+            case byDetails:
+                return (((CommonPersonObjectClient)client).getDetails().get(fieldname)!=null?((CommonPersonObjectClient)client).getDetails().get(fieldname):"").contains(criteria);
+        }
+        return false;
+    }
+}
