f1b928270fec4bfffd22dc6f96f8b0e140ed7fc8
raihan <raihan@mpower-social.com>
2015-09-15 16:28:00 +0600

    RA|added common domain profile image

---
+++ opensrp-app/src/main/java/org/ei/opensrp/domain/ProfileImage.java
@@ -0,0 +1,67 @@
+package org.ei.opensrp.domain;
+
+public class ProfileImage {
+    private String imageid;
+    private String anmId;
+    private String entityID;
+    private String contenttype;
+    private  String filepath;
+    private  String syncStatus;
+
+    public ProfileImage(String imageid, String anmId, String entityID, String contenttype, String filepath, String syncStatus) {
+        this.imageid = imageid;
+        this.entityID = entityID;
+        this.anmId = anmId;
+        this.contenttype = contenttype;
+        this.filepath = filepath;
+        this.syncStatus = syncStatus;
+    }
+
+    public String getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(String imageid) {
+        this.imageid = imageid;
+    }
+
+    public String getAnmId() {
+        return anmId;
+    }
+
+    public void setAnmId(String anmId) {
+        this.anmId = anmId;
+    }
+
+    public String getEntityID() {
+        return entityID;
+    }
+
+    public void setEntityID(String entityID) {
+        this.entityID = entityID;
+    }
+
+    public String getContenttype() {
+        return contenttype;
+    }
+
+    public void setContenttype(String contenttype) {
+        this.contenttype = contenttype;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getSyncStatus() {
+        return syncStatus;
+    }
+
+    public void setSyncStatus(String syncStatus) {
+        this.syncStatus = syncStatus;
+    }
+}
