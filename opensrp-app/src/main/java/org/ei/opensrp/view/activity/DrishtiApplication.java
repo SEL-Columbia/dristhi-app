07a9d5060c2197fa27b025c2eef653f8c4ac759f
Koros <korosmatick@gmail.com>
2016-02-03 13:06:43 +0300

    KG | Prevent the app from crashing after session expiry #142

--- opensrp-app/src/main/java/org/ei/opensrp/view/activity/DrishtiApplication.java
+++ opensrp-app/src/main/java/org/ei/opensrp/view/activity/DrishtiApplication.java
@@ -1,7 +1,10 @@
 package org.ei.opensrp.view.activity;
 
 import android.app.Application;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.util.Log;
+
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
@@ -13,56 +16,18 @@ import java.util.Locale;
 
 import static org.ei.opensrp.util.Log.logInfo;
 
-@ReportsCrashes(
-        formKey = "",
-        formUri = "https://drishtiapp.cloudant.com/acra-drishtiapp/_design/acra-storage/_update/report",
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.POST,
-        formUriBasicAuthLogin = "sompleakereepeavoldiftle",
-        formUriBasicAuthPassword = "ecUMrMeTKf1X1ODxHqo3b43W",
-        mode = ReportingInteractionMode.SILENT
-)
 public class DrishtiApplication extends Application {
-    private Locale locale = null;
-    private Context context;
+    private static final String TAG = "DrishtiApplication";
+
+    protected Locale locale = null;
+    protected Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        DrishtiSyncScheduler.setReceiverClass(SyncBroadcastReceiver.class);
-        ACRA.init(this);
-
-        context = Context.getInstance();
-        context.updateApplicationContext(getApplicationContext());
-        applyUserLanguagePreference();
-        cleanUpSyncState();
     }
 
-    private void cleanUpSyncState() {
-        DrishtiSyncScheduler.stop(getApplicationContext());
-        context.allSharedPreferences().saveIsSyncInProgress(false);
-    }
-
-    @Override
-    public void onTerminate() {
-        logInfo("Application is terminating. Stopping Dristhi Sync scheduler and resetting isSyncInProgress setting.");
-        cleanUpSyncState();
-    }
-
-    private void applyUserLanguagePreference() {
-        Configuration config = getBaseContext().getResources().getConfiguration();
-
-        String lang = context.allSharedPreferences().fetchLanguagePreference();
-        if (!"".equals(lang) && !config.locale.getLanguage().equals(lang)) {
-            locale = new Locale(lang);
-            updateConfiguration(config);
-        }
-    }
-
-    private void updateConfiguration(Configuration config) {
-        config.locale = locale;
-        Locale.setDefault(locale);
-        getBaseContext().getResources().updateConfiguration(config,
-                getBaseContext().getResources().getDisplayMetrics());
+    public void logoutCurrentUser(){
+        Log.e(TAG, "Child classes should implement this function");
     }
 }
