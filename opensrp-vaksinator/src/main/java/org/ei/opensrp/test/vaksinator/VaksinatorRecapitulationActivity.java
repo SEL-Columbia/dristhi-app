5453328a3546ae5775951708150423e9063fe039
MarwanNulhakam <atosoft.null@gmail.com>
2016-07-19 06:53:10 +0800

    MN | create data load and recapitulation feature, and create a textview controller to recapitulation layout

--- opensrp-vaksinator/src/main/java/org/ei/opensrp/test/vaksinator/VaksinatorRecapitulationActivity.java
+++ opensrp-vaksinator/src/main/java/org/ei/opensrp/test/vaksinator/VaksinatorRecapitulationActivity.java
@@ -1,15 +1,12 @@
 package org.ei.opensrp.test.vaksinator;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import org.ei.opensrp.Context;
 import org.ei.opensrp.commonregistry.CommonPersonObjectClient;
 import org.ei.opensrp.test.R;
+
 
 /**
  * Created by Iq on 09/06/16, modified by Marwan on 14/07/16
@@ -30,77 +27,63 @@ public class VaksinatorRecapitulationActivity extends Activity {
         Context context = Context.getInstance();
         setContentView(R.layout.smart_register_jurim_detail_client);
 
-        //profile
-        TextView nama = (TextView) findViewById(R.id.childName);
-        TextView motherName = (TextView) findViewById(R.id.motherName);
-        //TextView fatherName = (TextView) findViewById(R.id.fatherName);
-        TextView posyandu = (TextView) findViewById(R.id.posyandu);
-        TextView village = (TextView) findViewById(R.id.village);
-        TextView dateOfBirth = (TextView) findViewById(R.id.dateOfBirth);
-        TextView birthWeight = (TextView) findViewById(R.id.birthWeight);
-        //TextView currentWeight = (TextView) findViewById(R.id.currentWeight);
+        Context otherContext = Context.getInstance().updateApplicationContext(this.getApplicationContext());
 
-        //vaccination date
-        TextView hb1Under7 = (TextView) findViewById(R.id.hb1under7);
-        TextView hb1After7 = (TextView) findViewById(R.id.hb1after7);
-        TextView bcg = (TextView) findViewById(R.id.bcg);
-        TextView pol1 = (TextView) findViewById(R.id.pol1);
-        TextView dpt1 = (TextView) findViewById(R.id.dpt1);
-        TextView pol2 = (TextView) findViewById(R.id.pol2);
-        TextView dpt2 = (TextView) findViewById(R.id.dpt2);
-        TextView pol3 = (TextView) findViewById(R.id.pol3);
-        TextView dpt3 = (TextView) findViewById(R.id.dpt3);
-        TextView pol4 = (TextView) findViewById(R.id.pol4);
-        TextView measles = (TextView) findViewById(R.id.measles);
-        TextView mutationUnder30 = (TextView) findViewById(R.id.mutationUnder30);
-        TextView mutationAfter30 = (TextView) findViewById(R.id.mutationAfter30);
-        TextView mutationMoving = (TextView) findViewById(R.id.mutationMoving);
-        TextView complete = (TextView) findViewById(R.id.complete);
-        TextView additionalDPT = (TextView) findViewById(R.id.additionalDPT);
-        TextView additionalMeasles = (TextView) findViewById(R.id.additionalMeasles);
-        ImageView photo = (ImageView)findViewById(R.id.photo);
+        org.ei.opensrp.commonregistry.CommonPersonObjectController data = new org.ei.opensrp.commonregistry.CommonPersonObjectController(otherContext.allCommonsRepositoryobjects("anak"),
+                otherContext.allBeneficiaries(), otherContext.listCache(),
+                otherContext.personObjectClientsCache(), "nama_bayi", "anak", "nama_orang_tua", org.ei.opensrp.commonregistry.CommonPersonObjectController.ByColumnAndByDetails.byDetails);
 
-        ImageButton backButton = (ImageButton) findViewById(R.id.btn_back_to_home);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                startActivity(new Intent(VaksinatorRecapitulationActivity.this, VaksinatorSmartRegisterActivity.class));
-                overridePendingTransition(0, 0);
-            }
-        });
+        org.ei.opensrp.commonregistry.CommonPersonObjectClients clients = data.getClients();
+        TextView hbunder7 = (TextView) findViewById(R.id.hbUnder7Reporting);
+        TextView hbover7 = (TextView) findViewById(R.id.hbOver7Reporting);
+        TextView bcg = (TextView) findViewById(R.id.bcgReporting);
+        TextView pol1 = (TextView) findViewById(R.id.pol1Reporting);
+        TextView hb1 = (TextView) findViewById(R.id.hb1Reporting);
+        TextView pol2 = (TextView) findViewById(R.id.pol2Reporting);
+        TextView hb2 = (TextView) findViewById(R.id.hb2Reporting);
+        TextView pol3 = (TextView) findViewById(R.id.pol3Reporting);
+        TextView hb3 = (TextView) findViewById(R.id.hb3Reporting);
+        TextView pol4 = (TextView) findViewById(R.id.pol4Reporting);
+        TextView ipv = (TextView) findViewById(R.id.ipvReporting);
+        TextView measles = (TextView) findViewById(R.id.measlesReporting);
+        TextView diedu30 = (TextView) findViewById(R.id.diedunder30Reporting);
+        TextView diedo30 = (TextView) findViewById(R.id.diedover30Reporting);
+        TextView moving = (TextView) findViewById(R.id.movingReporting);
 
-        nama.setText(": "+(controller.getDetails().get("nama_bayi") != null ? controller.getDetails().get("nama_bayi") : "-"));
-        motherName.setText(": "+(controller.getDetails().get("nama_orang_tua") != null ? controller.getDetails().get("nama_orang_tua") : "-"));
-        //fatherName.setText(": "+(controller.getDetails().get("fatherName") != null? controller.getDetails().get("fatherName"):"-"));
-        village.setText(": "+(controller.getDetails().get("village") != null? controller.getDetails().get("village"):"-"));
-        posyandu.setText(": "+(controller.getDetails().get("nama_lokasi") != null? controller.getDetails().get("nama_lokasi"):"-"));
-        dateOfBirth.setText(": "+(controller.getDetails().get("tanggal_lahir") != null? controller.getDetails().get("tanggal_lahir"):"-"));
-        birthWeight.setText(": "+(controller.getDetails().get("berat_badan_saat_lahir") != null? controller.getDetails().get("berat_badan_saat_lahir"):"-"));
-        //currentWeight.setText(": "+(controller.getDetails().get("currentWeight") != null? controller.getDetails().get("currentWeight"):"-"));
+        int counter = 0;
 
-        hb1Under7.setText(": "+(controller.getDetails().get("hb1_kurang_7_hari") != null? controller.getDetails().get("hb1_kurang_7_hari"):"-"));
-        hb1After7.setText(": "+(controller.getDetails().get("hb1_lebih_7_hari") != null? controller.getDetails().get("hb1_lebih_7_hari"):"-"));
-        bcg.setText(": "+(controller.getDetails().get("bcg_pol_1") != null? controller.getDetails().get("bcg_pol_1"):"-"));
-        pol1.setText(": "+(controller.getDetails().get("bcg_pol_1") != null? controller.getDetails().get("bcg_pol_1"):"-"));
-        dpt1.setText(": "+(controller.getDetails().get("dpt_1_pol_2") != null? controller.getDetails().get("dpt_1_pol_2"):"-"));
-        pol2.setText(": "+(controller.getDetails().get("dpt_1_pol_2") != null? controller.getDetails().get("dpt_1_pol_2"):"-"));
-        dpt2.setText(": "+(controller.getDetails().get("dpt_2_pol_3") != null? controller.getDetails().get("dpt_2_pol_3"):"-"));
-        pol3.setText(": "+(controller.getDetails().get("dpt_2_pol_3") != null? controller.getDetails().get("dpt_2_pol_3"):"-"));
-        dpt3.setText(": "+(controller.getDetails().get("dpt_3_pol_4_ipv") != null? controller.getDetails().get("dpt_3_pol_4_ipv"):"-"));
-        pol4.setText(": "+(controller.getDetails().get("dpt_3_pol_4_ipv") != null? controller.getDetails().get("dpt_3_pol_4_ipv"):"-"));
-        measles.setText(": "+(controller.getDetails().get("imunisasi_campak") != null? controller.getDetails().get("imunisasi_campak"):"-"));
-        mutationUnder30.setText(": "+(controller.getDetails().get("mutasi_meninggal_kurang_30hari") != null? controller.getDetails().get("mutasi_meninggal_kurang_30hari"):"-"));
-        mutationAfter30.setText(": "+(controller.getDetails().get("mutasi_meninggal_lebih_30hari") != null? controller.getDetails().get("mutasi_meninggal_lebih_30hari"):"-"));
-        mutationMoving.setText(": "+(controller.getDetails().get("tanggal_pindah") != null? controller.getDetails().get("tanggal_pindah"):"-"));
-        complete.setText(": "+(controller.getDetails().get("imunisasi_lengkap") != null? controller.getDetails().get("imunisasi_lengkap"):"-"));
-        additionalDPT.setText(": " + (controller.getDetails().get("dpt_hb_campak_lanjutan") != null ? controller.getDetails().get("dpt_hb_campak_lanjutan") : "-"));
-        additionalMeasles.setText(": " + (controller.getDetails().get("dpt_hb_campak_lanjutan") != null ? controller.getDetails().get("dpt_hb_campak_lanjutan") : "-"));
+        counter = recapitulation(clients,"hb1_kurang_7_hari","-");
+        hbunder7.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"hb1_lebih_7_hari","-");
+        hbover7.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"bcg_pol_1","-");
+        bcg.setText(Integer.toString(counter));
+        pol1.setText(counter);
+        counter = recapitulation(clients,"dpt_1_pol_2","-");
+        hb1.setText(Integer.toString(counter));
+        pol2.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"dpt_2_pol_3","-");
+        hb2.setText(Integer.toString(counter));
+        pol3.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"dpt_3_pol_4_ipv","-");
+        hb3.setText(Integer.toString(counter));
+        pol4.setText(Integer.toString(counter));
+        ipv.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"imunisasi_campak","-");
+        measles.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"mutasi_meninggal_kurang_30hari","-");
+        diedu30.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"mutasi_meninggal_lebih_30hari","-");
+        diedo30.setText(Integer.toString(counter));
+        counter = recapitulation(clients,"tanggal_pindah","-");
+        moving.setText(Integer.toString(counter));
+    }
 
-        if(controller.getDetails().get("jenis_kelamin").contains("l")){
-            photo.setImageResource(R.drawable.child_boy_infant);
-        }else{
-            photo.setImageResource(R.drawable.child_girl_infant);
+    private int recapitulation(org.ei.opensrp.commonregistry.CommonPersonObjectClients clients,String fieldName, String keyword){
+        int counter = 0;
+        for(int i=0;i<clients.size();i++){
+            counter = ((org.ei.opensrp.commonregistry.CommonPersonObjectClient)clients.get(i)).getDetails().get(fieldName).equals(keyword) ? counter+1:counter;
         }
+        return counter;
     }
 }
