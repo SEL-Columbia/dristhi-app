b7f2f2ecf5500ad8e2b4cc9229b09454896fd09a
MarwanNulhakam <atosoft.null@gmail.com>
2016-08-31 18:56:55 +0800

    MN | Clean some pull conflict

--- opensrp-app/assets/web/forms/js_include.js
+++ opensrp-app/assets/web/forms/js_include.js
@@ -1,17 +1,5 @@
 <script type="text/javascript">
-<<<<<<< HEAD
-         var loadDraft; // our function to initialize a form with passed data
-         var savePartialData; // function to be triggered when user is exits the form to save the partially filled form
-         var settings = {};
-         settings['supportEmail'] = 'support@enketo.org';
-         settings['serverURL'] = 'https://ona.io/opensrp';
-         settings['formId'] = 'FWNewHH'; //inject this from code as well
-         settings['modernBrowsersURL'] = '#';
-
-         var modelStr = "$model_string_placeholder"; //injected from code
-=======
 
     var globalModelStr = "$model_string_placeholder"; //injected from code
->>>>>>> fc57a485ae9e44237dc69626e10ad144281a146a
 
 </script>
