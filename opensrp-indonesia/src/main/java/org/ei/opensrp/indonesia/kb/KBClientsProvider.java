a033fbed0726af723459f07505b84b613cba54cf
mib-iqbal <mib-iqbaal@gmail.com>
2016-09-28 15:22:11 +0800

    MI | removing unused commented code in layout - bidan app #263

--- opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/kb/KBClientsProvider.java
+++ opensrp-indonesia/src/main/java/org/ei/opensrp/indonesia/kb/KBClientsProvider.java
@@ -168,7 +168,6 @@ public class KBClientsProvider implements SmartRegisterCLientsProviderForCursorA
         viewHolder.village_name.setText(pc.getDetails().get("desa")!=null?pc.getDetails().get("desa"):"");
         viewHolder.wife_age.setText(pc.getColumnmaps().get("umur")!=null?pc.getColumnmaps().get("umur"):"");
         viewHolder.no_ibu.setText(pc.getColumnmaps().get("noIbu")!=null?pc.getColumnmaps().get("noIbu"):"");
-     //   viewHolder.unique_id.setText(pc.getDetails().get("unique_id")!=null?pc.getDetails().get("unique_id"):"");
 
         viewHolder.gravida.setText(pc.getDetails().get("gravida")!=null?pc.getDetails().get("gravida"):"-");
         viewHolder.parity.setText(pc.getDetails().get("partus")!=null?pc.getDetails().get("partus"):"-");
